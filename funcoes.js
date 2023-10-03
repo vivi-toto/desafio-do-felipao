5e8a641e367898ca235685125e8bfe4899f65cd6
vivi-toto <fernandavitoria856@gmail.com>
2023-10-02 23:06:01 -0400

    novo exemplo do felipao

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+function enviarDados () {
+    let nomeBanco = "banco-de-dados";
+    console.log("Salvando dados em : " + nomeBanco)
+
+    if (3 === 3) {
+        console.log("senha validada");
+    }
+}
