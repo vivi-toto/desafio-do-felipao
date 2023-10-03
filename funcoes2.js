7766ae610e4c627c80770f96c56fc19fe0632dd2
vivi-toto <fernandavitoria856@gmail.com>
2023-10-03 00:09:08 -0400

    novo exercicio

--- funcoes2.js
+++ funcoes2.js
@@ -1,1 +1,7 @@
-console.log("olá mundo")
+torrar ("pão na chapa", "Fernanda") 
+
+function torrar (pao, nome, valor=99.90) {
+    console.log("torrada feita com " + pao);
+    console.log("ele é um pedido de  " + nome);
+    console.log("O valor total é " + valor);
+}
